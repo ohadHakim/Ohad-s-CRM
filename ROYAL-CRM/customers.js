9a28a73447aafa91ba557759df23306334683503
Ohad Hakim <hakimohad@gmail.com>
2022-05-04 15:39:07 +0300

    changes and configuration

--- ROYAL-CRM/customers.js
+++ ROYAL-CRM/customers.js
@@ -1,20 +1,60 @@
-const customers = {
-  list: [],
+const mysql = require("mysql2");
+const config = require("./config/dev");
 
-  addCostumers: function () {
-    const name = process.argv.slice(2);
+const pool = mysql.createPool({
+  host: config.DB_HOST,
+  user: config.DB_USER,
+  password: config.DB_PASSWORD,
+  database: config.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+});
+
+const customers = {
+  // list: [],
+
+  addCostumers: function (name, phone, email, country_id) {
+    // const name = process.argv.slice(2);
     if (!name || name.length === 0) {
       throw `ERROR: name is empty`;
     }
 
-    this.list.push({
-      name: name,
-      id: this.list.length,
+    // this.list.push({
+    //   name: name,
+    //   id: this.list.length,
+    // });
+
+    pool.getConnection(function (connErr, connection) {
+      if (connErr) throw connErr;
+      const sql =
+        "INSERT INTO customers(name,phone,email,country_id) VALUES (?,?,?,?)";
+
+      connection.query(
+        sql,
+        [name, phone, email, country_id],
+        function (sqlErr, result, fields) {
+          if (sqlErr) throw sqlErr;
+          console.log(fields);
+          console.log(result);
+        }
+      );
     });
   },
-  customersList: function () {
-    this.list.forEach((customer) => {
-      console.log(`ok. name: ${customer.name} was created.`);
+  customersList: function (req, res) {
+    // this.list.forEach((customer) => {
+    //   console.log(`ok. name: ${customer.name} was created.`);
+    // });
+
+    pool.getConnection(function (connErr, connection) {
+      if (connErr) throw connErr;
+      const sql = "SELECT * FROM customers";
+
+      connection.query(sql, function (sqlErr, result, fields) {
+        if (sqlErr) throw sqlErr;
+
+        return res.send(result);
+      });
     });
   },
 };
--- ROYAL-CRM/routes/index.js
+++ ROYAL-CRM/routes/index.js
@@ -1,9 +1,12 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const customersModule = require("../customers");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  // res.render("index", { title: "Express" });
+  customersModule.addCostumers("Lola", "052565656", "gl@gmail.com", 1);
+  customersModule.customersList(req, res);
 });
 
 module.exports = router;
